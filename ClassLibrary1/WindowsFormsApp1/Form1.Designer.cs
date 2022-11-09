900628723d879e37fa7344f357ee1142ca52e188
Franco44305 <Franco44305@gmail.com>
2022-11-09 07:12:43 -0300

    Vista ADO.NET

---
+++ ClassLibrary1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Tipo = new System.Windows.Forms.TextBox();
+            this.Nombre = new System.Windows.Forms.TextBox();
+            this.Numero = new System.Windows.Forms.TextBox();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-1, -1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(801, 298);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.linkLabel1.Location = new System.Drawing.Point(64, 316);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(87, 16);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Ejecutar vista";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.linkLabel2.Location = new System.Drawing.Point(316, 316);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(157, 16);
+            this.linkLabel2.TabIndex = 2;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Ejecutar Store Procedure";
+            this.linkLabel2.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(41, 358);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 60);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Vista";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(330, 358);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 60);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Cargar paciente";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Tipo
+            // 
+            this.Tipo.Location = new System.Drawing.Point(644, 377);
+            this.Tipo.Name = "Tipo";
+            this.Tipo.Size = new System.Drawing.Size(144, 22);
+            this.Tipo.TabIndex = 5;
+            // 
+            // Nombre
+            // 
+            this.Nombre.Location = new System.Drawing.Point(644, 338);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(144, 22);
+            this.Nombre.TabIndex = 6;
+            // 
+            // Numero
+            // 
+            this.Numero.Location = new System.Drawing.Point(644, 416);
+            this.Numero.Name = "Numero";
+            this.Numero.Size = new System.Drawing.Size(144, 22);
+            this.Numero.TabIndex = 7;
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Location = new System.Drawing.Point(522, 337);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(0, 16);
+            this.linkLabel3.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(567, 341);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(479, 419);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Numero de documento:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(499, 383);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Tipo de documento:";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button3.Location = new System.Drawing.Point(215, 407);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 31);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Volver";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.Numero);
+            this.Controls.Add(this.Nombre);
+            this.Controls.Add(this.Tipo);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox Tipo;
+        private System.Windows.Forms.TextBox Nombre;
+        private System.Windows.Forms.TextBox Numero;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ClassLibrary1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Conexion(string consulta)
+        {
+            var conString = ConfigurationManager.ConnectionStrings["dbSql"].ConnectionString;
+            try
+            {
+                using (SqlConnection Conector = new SqlConnection(conString))
+                {
+                    Conector.Open();
+                    DataTable dt = new DataTable();
+                    string query = consulta;
+                    SqlCommand comando = new SqlCommand(query, Conector);
+                    SqlDataAdapter da = new SqlDataAdapter(comando);
+                    da.SelectCommand = comando;
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void EjecutarStoredProcedure()
+        {
+            var conString = ConfigurationManager.ConnectionStrings["dbSql"].ConnectionString;
+            try
+            {
+                using (SqlConnection conector = new SqlConnection(conString))
+                {
+                    conector.Open();
+                    string query = @"AddPaciente";
+                    SqlCommand comando = new SqlCommand(query, conector);
+                    SqlDataAdapter adapter = new SqlDataAdapter(comando);
+                    comando.CommandType = CommandType.StoredProcedure;
+                    comando.Parameters.Add("@Nombre", SqlDbType.VarChar).Value = Nombre.Text;
+                    comando.Parameters.Add("@Tipo_Nro_Documento", SqlDbType.VarChar).Value = Tipo.Text;
+                    comando.Parameters.Add("@Nro_Documento", SqlDbType.Int).Value = int.Parse(Numero.Text);
+                    comando.ExecuteNonQuery();
+                    MessageBox.Show("Carga correcta");
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Conexion("Select * from Pacientes");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Conexion("Select * from Vista_Saldo_Pacientes");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Conexion("Select * from Pacientes");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EjecutarStoredProcedure();
+        }
+    }
+}
